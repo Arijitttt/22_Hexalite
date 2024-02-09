785e35cd701ebb6a0620d67e01776e352adaed2a
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-02-09 10:55:29 +0530

    react deleted

--- HackFest-React/src/Map_Recomandation/Provider.jsx
+++
@@ -1,6 +0,0 @@
-export default{
-    maptiler:{
-        url:'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }
-}
